e883ea9b878ea09d1c3e47d1eafe33ebcabe38e2
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-04-18 16:36:16 +0100

    Updated about you and about your partner

--- app/views/claim-management/version-1/routes.js
+++ app/views/claim-management/version-1/routes.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/about-you`)
 })
 
+router.post('/about-you', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/about-your-partner`)
+})
+
+router.post('/about-your-partner', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/about-your-children`)
+})
+
 module.exports = router
