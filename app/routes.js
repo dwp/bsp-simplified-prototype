3821b0e4e3e3e4c3c1da97fe8e284d5f75e22c19
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-04-18 13:01:11 +0100

    Migrated bsp-simplified-prototype

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,19 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
 
-// Route index page
-router.get('/', function (req, res) {
+const router = new express.Router()
+
+router.get('/', (req, res) => {
   res.render('index')
 })
 
-// add your routes here
+// Payment Scheduler
+router.use('/payment-scheduler/version-1', require('./views/payment-scheduler/version-1/routes'))
+router.use('/payment-scheduler/version-2', require('./views/payment-scheduler/version-2/routes'))
+router.use('/payment-scheduler/version-3', require('./views/payment-scheduler/version-3/routes'))
+router.use('/payment-scheduler/version-4', require('./views/payment-scheduler/version-4/routes'))
+router.use('/payment-scheduler/version-5', require('./views/payment-scheduler/version-5/routes'))
+
+// Claim Management
+router.use('/claim-management/version-1', require('./views/claim-management/version-1/routes'))
 
 module.exports = router
