7cbd5c345a899e2e2775337e1d81152dbdbadd45
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-04-19 11:20:30 +0100

    Updated paper claim process

--- app/views/claim-management/version-1/routes.js
+++ app/views/claim-management/version-1/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = new express.Router()
 
 router.get('/', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/about-you`)
+  res.redirect(`/${req.feature}/${req.sprint}/home`)
 })
 
 router.post('/about-you', (req, res) => {
@@ -14,4 +14,11 @@ router.post('/about-your-partner', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/about-your-children`)
 })
 
+router.post('/about-your-children', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/payment-details`)
+})
+
+router.post('/payment-details', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/home`)
+})
 module.exports = router
