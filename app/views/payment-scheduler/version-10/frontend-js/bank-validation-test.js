29c5f85d3f507c8575473c6a11d5d15bc32d88b3
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-05-25 14:44:54 +0100

    updated to work with iBan

--- app/views/payment-scheduler/version-10/frontend-js/bank-validation-test.js
+++ app/views/payment-scheduler/version-10/frontend-js/bank-validation-test.js
@@ -1,21 +1,23 @@
 var throwError = throwError || $('form:first').attr('data-throwError')
 
 $(document).on('submit', 'form', function (e) {
-  var $accountNumber = $('#input-accountNumber')
-  var accountNumberLength = $accountNumber.val().length
+  var dataEntered = $('#input-accountNumber').val().length + $('#input-ibanNumber').val().length
 
-  if (accountNumberLength >= 8 && throwError === 'Yes') {
+  if (dataEntered > 0 && throwError === 'Yes') {
     e.preventDefault()
-
     setTimeout(function () {
       $(document).scrollTop('body', 0)
       appendErrorBanner()
-      appendErrors('#input-sortCode')
-      appendErrors('#input-accountNumber')
+      appendErrors([
+        '#input-sortCode',
+        '#input-accountNumber',
+        '#input-bankCode',
+        '#input-ibanNumber'
+      ])
 
-      $accountNumber.val(function () {
-        return $(this).val().replace(/.$/, 7)
-      })
+      editInputVal('#input-accountNumber')
+      editInputVal('#input-ibanNumber')
+
       throwError = 'No'
     }, 500)
   }
@@ -40,11 +42,18 @@ function appendErrorBanner () {
     '</div>'
   )
 }
-function appendErrors (input) {
-  $(input).closest('.form-group').addClass('form-group-error')
-  $(input).closest('.form-group').find('.form-label').append(
-    '<span class="error-message">' +
-    '  Enter these details again' +
-    '</span>'
-  )
+function appendErrors (array) {
+  for (var i = 0; i < array.length; i++) {
+    $(array[i]).closest('.form-group').addClass('form-group-error')
+    $(array[i]).closest('.form-group').find('.form-label').append(
+      '<span class="error-message">' +
+      '  Enter these details again' +
+      '</span>'
+    )
+  }
+}
+function editInputVal (input) {
+  $(input).val(function () {
+    return $(this).val().replace(/.$/, 7)
+  })
 }
