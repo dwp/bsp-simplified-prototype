f20395afa1279727d31aa7349a192a51eb88b4bc
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-05-30 10:53:00 +0100

    Updated version 11

--- app/views/payment-scheduler/version-11/routes.js
+++ app/views/payment-scheduler/version-11/routes.js
@@ -3,33 +3,12 @@ const express = require('express')
 const router = new express.Router()
 
 router.get('/', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/have-any-payments-been-made-manually`)
+  res.redirect(`/${req.feature}/${req.sprint}/find-a-payment-schedule`)
 })
 
-router.post('/have-any-payments-been-made-manually', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/have-all-payments-been-made`)
-})
-
-router.post('/have-all-payments-been-made', (req, res) => {
-  if (req.body['payments-all-made'] === 'No') {
-    return res.redirect(`/${req.feature}/${req.sprint}/you-must-make-all-payments`)
-  }
-  res.redirect(`/${req.feature}/${req.sprint}/set-up-a-payment-schedule`)
-})
-
-router.post('/set-up-a-payment-schedule', (req, res) => {
-  if (req.body.nino === 'AB123456C') {
-    return res.redirect(`/${req.feature}/${req.sprint}/duplicate-claim`)
-  }
-  res.redirect(`/${req.feature}/${req.sprint}/confirm-details`)
-})
-
-router.post('/confirm-details', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/schedule-created`)
-})
-
-router.post('/confirm-details', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/when-was-the-last-manual-payment-made`)
+router.get('/find-a-payment-schedule', (req, res) => {
+  const nino = req.query.nino
+  res.render(`${req.feature}/${req.sprint}/find-a-payment-schedule`, {nino})
 })
 
 module.exports = router
