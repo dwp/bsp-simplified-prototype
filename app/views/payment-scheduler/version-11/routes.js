07367e1fbf64a34072fb2ab4113c18e5b6817adf
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-05-30 16:19:37 +0100

    Updated rate change journey

--- app/views/payment-scheduler/version-11/routes.js
+++ app/views/payment-scheduler/version-11/routes.js
@@ -11,4 +11,15 @@ router.get('/find-a-payment-schedule', (req, res) => {
   res.render(`${req.feature}/${req.sprint}/find-a-payment-schedule`, {nino})
 })
 
+router.post('/change-payment-details', (req, res) => {
+  if (req.body['which-details-do-you-want-to-change'] === 'rate') {
+    return res.redirect(`/${req.feature}/${req.sprint}/change-rate`)
+  }
+  res.redirect(`/${req.feature}/${req.sprint}/change-bank-details`)
+})
+
+router.post('/change-rate', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/schedule`)
+})
+
 module.exports = router
