dd6efac7486f5570748c81d284eaa6c60bc961b5
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-10-18 19:57:47 +0100

    Updated claim capture

--- app/views/claim-capture/version-1/routes.js
+++ app/views/claim-capture/version-1/routes.js
@@ -17,6 +17,11 @@ router.post('/settings', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/find-a-claim`)
 })
 
+router.get('/find-a-claim', (req, res) => {
+  const search = req.query.findNino.toUpperCase()
+  res.render(`${req.feature}/${req.sprint}/find-a-claim`, {search})
+})
+
 router.get('/start-new-claim', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/claim-date`)
 })
@@ -26,6 +31,10 @@ router.post('/claim-date', (req, res) => {
 })
 
 router.post('/claimant-details', (req, res) => {
+  const birthYear = parseInt(req.body.claimant.dateOfBirth.year)
+  if (birthYear < 1952) {
+    return res.redirect(`/${req.feature}/${req.sprint}/rejected-ineligible`)
+  }
   res.redirect(`/${req.feature}/${req.sprint}/partner-details`)
 })
 
@@ -34,11 +43,19 @@ router.post('/partner-details', (req, res) => {
 })
 
 router.post('/children-details', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/payment-details`)
+  const marriageVerified = req.session.data.verification['marriage-verified']
+  if (marriageVerified === 'Yes') {
+    return res.redirect(`/${req.feature}/${req.sprint}/payment-details`)
+  }
+  res.redirect(`/${req.feature}/${req.sprint}/pause-claim`)
 })
 
 router.post('/payment-details', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/confirm-details`)
+})
+
+router.post('/pause-claim', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/paused-marriage`)
 })
 
 module.exports = router
