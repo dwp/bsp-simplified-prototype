bb6c998bbc0d8c74fcc289aa5de6e20ebd6900cc
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-11-10 15:49:00 +0000

    Started task list pattern

--- app/views/claim-capture-list/version-1/routes.js
+++ app/views/claim-capture-list/version-1/routes.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const {clearFormData} = require('./common/functions')
 
 const router = new express.Router()
 
@@ -17,7 +16,7 @@ router.get('/', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/settings`)
 })
 router.post('/settings', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/process-a-claim`)
+  res.redirect(`/${req.feature}/${req.sprint}/create-new-claim`)
 })
 
 // -----------------------------------------------------------------------------
@@ -30,8 +29,52 @@ router.get('/find-a-claim', (req, res) => {
 })
 
 // -----------------------------------------------------------------------------
-// Enter a new claim -----------------------------------------------------------
+// Create new claim ------------------------------------------------------------
 // -----------------------------------------------------------------------------
+router.get('/create-new-claim', (req, res) => {
+  res.render(`${req.feature}/${req.sprint}/create-new-claim/create-new-claim`)
+})
+router.post('/create-new-claim', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/claim/1/paused`)
+})
 
+// -----------------------------------------------------------------------------
+// Claim -----------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/claim/:id/:status', (req, res) => {
+  const id = req.params.id
+  const status = req.params.status
+  res.render(`${req.feature}/${req.sprint}/claim/claim-paused`, {id, status})
+})
+
+// -----------------------------------------------------------------------------
+// Capture ---------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/capture/:id/:page', (req, res) => {
+  const id = req.params.id
+  const page = req.params.page
+  res.render(`${req.feature}/${req.sprint}/capture/${page}`, {id})
+})
+router.post('/capture/:id/:page', (req, res) => {
+  const id = req.params.id
+  addToLog(req, 'capture')
+  res.redirect(`/${req.feature}/${req.sprint}/claim/${id}/paused`)
+})
+
+function addToLog (req, type) {
+  const log = req.session.data.log || []
+  const page = req.params.page
+  if (type === 'capture') {
+    const details = page.split('-')
+    const message = `${capitalizeFirstLetter(details[0])} ${details[1]} entered`
+    log.push(message)
+  }
+  const set = Array.from(new Set(log))
+  req.session.data.log = set
+}
+
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 
 module.exports = router
