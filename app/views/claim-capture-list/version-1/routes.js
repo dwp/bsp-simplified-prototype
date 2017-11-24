5caf3396e7cde40da37befdb9d8b452690abd02e
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2017-11-24 21:58:47 +0000

    Changed new version to be version 1

--- app/views/claim-capture-list/version-1/routes.js
+++ app/views/claim-capture-list/version-1/routes.js
@@ -1,154 +1,99 @@
 const express = require('express')
-
+const {logOnPost} = require('../../../../lib/utils')
 const router = new express.Router()
 
-router.use((req, res, next) => {
-  if (req.method === 'POST') {
-    console.log(JSON.stringify(req.session.data, null, 2))
-  }
-  next()
+// Log session to console on POST requests
+router.use(logOnPost)
+
+router.get('/', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/settings`)
 })
 
 // -----------------------------------------------------------------------------
 // Settings --------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-router.get('/', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/settings`)
+router.get('/settings', (req, res) => {
+  res.render(`${req.feature}/${req.sprint}/settings`)
 })
 router.post('/settings', (req, res) => {
   res.redirect(`/${req.feature}/${req.sprint}/start-a-new-claim`)
 })
 
 // -----------------------------------------------------------------------------
-// Find a claim ----------------------------------------------------------------
-// -----------------------------------------------------------------------------
-router.get('/find-a-claim', (req, res) => {
-  const nino = req.query.findNino
-  const search = nino ? nino.toUpperCase() : ''
-  res.render(`${req.feature}/${req.sprint}/find-a-claim/find-a-claim`, {search})
-})
-
-// -----------------------------------------------------------------------------
-// Start a new claim ------------------------------------------------------------
+// Start a new claim -----------------------------------------------------------
 // -----------------------------------------------------------------------------
 router.get('/start-a-new-claim', (req, res) => {
   res.render(`${req.feature}/${req.sprint}/start-a-new-claim/start-a-new-claim`)
 })
 router.post('/start-a-new-claim', (req, res) => {
-  const scenario = req.session.data.scenario || 1
-  res.redirect(`/${req.feature}/${req.sprint}/claim/${scenario}/paused`)
+  const scenario = req.session.data.scenario || '1'
+  res.redirect(`/${req.feature}/${req.sprint}/task-list/${scenario}`)
 })
 
 // -----------------------------------------------------------------------------
-// Claim -----------------------------------------------------------------------
+// Task list -------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-router.get('/claim/:id/:status', (req, res) => {
-  const id = req.params.id
-  const status = req.params.status
-  if (status === 'paused') {
-    return res.render(`${req.feature}/${req.sprint}/claim/claim-paused`, {id, status})
-  }
-  if (status === 'paused-tasks') {
-    return res.render(`${req.feature}/${req.sprint}/claim/claim-paused-tasks`, {id, status})
-  }
-  if (status === 'disallowed') {
-    return res.render(`${req.feature}/${req.sprint}/claim/claim-disallowed`, {id, status})
-  }
-  res.render(`${req.feature}/${req.sprint}/claim/claim-allowed`, {id, status})
+router.get('/task-list/:scenario', (req, res) => {
+  const scenario = req.params.scenario
+  res.render(`${req.feature}/${req.sprint}/task-list/task-list`, {scenario})
 })
 
 // -----------------------------------------------------------------------------
 // Capture ---------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-router.get('/capture/:id/:page', (req, res) => {
-  const id = req.params.id
-  const page = req.params.page
-  res.render(`${req.feature}/${req.sprint}/capture/${page}`, {id})
-})
-router.post('/capture/:id/:page', (req, res) => {
-  const id = req.params.id
-  const page = req.params.page
-  if (page === 'pause-claim') {
-    res.redirect(`/${req.feature}/${req.sprint}/decisions/${id}/paused`)    
-  }
+router.post('/capture/:page', (req, res) => {
+  const scenario = req.session.data.scenario || '1'
   addToLog(req, 'capture')
-  res.redirect(`/${req.feature}/${req.sprint}/claim/${id}/paused`)
+  res.redirect(`/${req.feature}/${req.sprint}/task-list/${scenario}`)
 })
-
-// -----------------------------------------------------------------------------
-// Capture ---------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-router.get('/add/:id/:page', (req, res) => {
-  const id = req.params.id
-  const page = req.params.page
-  res.render(`${req.feature}/${req.sprint}/capture/${page}`, {id})
-})
-router.post('/add/:id/:page', (req, res) => {
-  const id = req.params.id
-  addToLog(req, 'capture')
-  res.redirect(`/${req.feature}/${req.sprint}/claim/${id}/paused-tasks`)
-})
-
-// -----------------------------------------------------------------------------
-// Verify ----------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-router.get('/verify/:id/:page', (req, res) => {
-  const id = req.params.id
-  const page = req.params.page
-  res.render(`${req.feature}/${req.sprint}/verify/${page}`, {id})
-})
-router.post('/verify/:id/:page', (req, res) => {
-  const id = req.params.id
+router.post('/verify/:page', (req, res) => {
+  const scenario = req.session.data.scenario || '1'
   addToLog(req, 'verify')
-  res.redirect(`/${req.feature}/${req.sprint}/claim/${id}/paused`)
+  res.redirect(`/${req.feature}/${req.sprint}/task-list/${scenario}`)
 })
 
 // -----------------------------------------------------------------------------
-// Check and submit ------------------------------------------------------------
+// Confirm details -------------------------------------------------------------
 // -----------------------------------------------------------------------------
-router.get('/process/:id/check-and-submit', (req, res) => {
-  const id = req.params.id
-  res.render(`${req.feature}/${req.sprint}/check-and-submit/check-and-submit`, {id})
+router.get('/confirm-details/:scenario', (req, res) => {
+  const scenario = req.session.data.scenario || '1'
+  res.render(`${req.feature}/${req.sprint}/confirm-details/confirm-details`, {scenario})
 })
-router.post('/process/:id/check-and-submit', (req, res) => {
-  const id = req.params.id
-  const scenario = req.session.data.scenario
+router.post('/confirm-details/:scenario', (req, res) => {
+  const scenario = req.session.data.scenario || '1'
+  if (scenario === '1') {
+    return res.redirect(`/${req.feature}/${req.sprint}/decisions/${scenario}/allowed`)
+  }
   if (scenario === '2') {
-    return res.redirect(`/${req.feature}/${req.sprint}/decisions/${id}/disallowed`)
+    return res.redirect(`/${req.feature}/${req.sprint}/decisions/${scenario}/disallowed`)
   }
-  if (scenario === '3') {
-    return res.redirect(`/${req.feature}/${req.sprint}/decisions/${id}/pause`)
-  }
-  res.redirect(`/${req.feature}/${req.sprint}/decisions/${id}/allowed`)
+  res.redirect(`/${req.feature}/${req.sprint}/decisions/${scenario}/set-reminder`)
 })
 
 // -----------------------------------------------------------------------------
 // Decisions -------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-router.get('/decisions/:id/:status', (req, res) => {
-  const id = req.params.id
-  const status = req.params.status
-  if (status === 'allowed') {
-    return res.render(`${req.feature}/${req.sprint}/decisions/allowed`, {id})
-  }
-  if (status === 'disallowed') {
-    return res.render(`${req.feature}/${req.sprint}/decisions/disallowed`, {id})
-  }
-  if (status === 'pause') {
-    return res.redirect(`/${req.feature}/${req.sprint}/capture/${id}/pause-claim`)
-  }
-  if (status === 'paused') {
-    return res.render(`${req.feature}/${req.sprint}/decisions/paused-payment`, {id})
-  }
-  return res.render(`${req.feature}/${req.sprint}/decisions/allowed`, {id, status})
+router.get('/decisions/:scenario/:decision', (req, res) => {
+  const scenario = req.params.scenario
+  const decision = req.params.decision
+  res.render(`${req.feature}/${req.sprint}/decisions/${decision}`, {scenario})
+})
+
+// -----------------------------------------------------------------------------
+// Completed claims ------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/claim/:scenario/:decision', (req, res) => {
+  const scenario = req.params.scenario
+  const decision = req.params.decision
+  res.render(`${req.feature}/${req.sprint}/completed-claim/${decision}`, {scenario})
 })
 
 // -----------------------------------------------------------------------------
 // Schedule --------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-router.get('/schedule/:id', (req, res) => {
-  const id = req.params.id
-  res.render(`${req.feature}/${req.sprint}/schedule/schedule`, {id})
+router.get('/schedule/:scenario', (req, res) => {
+  const scenario = req.params.scenario
+  res.render(`${req.feature}/${req.sprint}/schedule/schedule`, {scenario})
 })
 
 // -----------------------------------------------------------------------------
