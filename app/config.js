c1cfc57a8b20431b375d2ec16b78516077bf6826
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-04-17 13:52:20 +0100

    Content changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'BSP Manager',
+  serviceName: 'Manage Bereavement Support Payment',
 
   // Default port that prototype runs on
   port: '3000',
