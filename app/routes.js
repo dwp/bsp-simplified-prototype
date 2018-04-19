798b3726fb9d961c4b028e3519331cff82325815
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-04-19 11:52:23 +0100

    Moved into new feature - integration

--- app/routes.js
+++ app/routes.js
@@ -17,25 +17,28 @@ router.get('/', (req, res) => {
 
 // Payment Scheduler
 router.use(/\/payment-scheduler\/version-([0-9]+)/, (req, res, next) => {
-  require(`./views/payment-scheduler/version-${req.params[0]}/routes`)(req, res, next);
+  return require(`./views/payment-scheduler/version-${req.params[0]}/routes`)(req, res, next);
 })
 
 // Claim Management
 router.use(/\/claim-management\/version-([0-9]+)/, (req, res, next) => {
-  require(`./views/claim-management/version-${req.params[0]}/routes`)(req, res, next);
+  return require(`./views/claim-management/version-${req.params[0]}/routes`)(req, res, next);
 })
 
 // Claim Capture
 router.use(/\/claim-capture\/version-([0-9]+)/, (req, res, next) => {
-  require(`./views/claim-capture/version-${req.params[0]}/routes`)(req, res, next);
+  return require(`./views/claim-capture/version-${req.params[0]}/routes`)(req, res, next);
 })
 router.use(/\/claim-capture-list\/version-([0-9]+)/, (req, res, next) => {
-  require(`./views/claim-capture-list/version-${req.params[0]}/routes`)(req, res, next);
+  return require(`./views/claim-capture-list/version-${req.params[0]}/routes`)(req, res, next);
+})
+router.use(/\/integration\/version-([0-9]+)/, (req, res, next) => {
+  return require(`./views/integration/version-${req.params[0]}/routes`)(req, res, next);
 })
 
 // Citizen Facing
 router.use(/\/citizen-facing\/version-([0-9]+)/, (req, res, next) => {
-  require(`./views/citizen-facing/version-${req.params[0]}/routes`)(req, res, next);
+  return require(`./views/citizen-facing/version-${req.params[0]}/routes`)(req, res, next);
 })
 
 module.exports = router
