fd218f96f7e09c2def1f0a352754804993a82078
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-05 13:36:24 +0100

    Added version 17

---
+++ app/views/claim-capture-list/version-17/routes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const path = require('path')
+const router = new express.Router()
+const {logOnPost} = require('../../../../lib/utils')
+
+// Log session to console on POST requests
+router.use(logOnPost)
+router.use('/load-pdf/', express.static(path.join(__dirname, '../version-15/_pdf')))
+
+router.get('/', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/start-a-new-claim/`)
+})
+
+// -----------------------------------------------------------------------------
+// Find a claim ----------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/find-a-claim', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  const d = require(`./_dummy-data/${scenario}.json`)
+  const nino = req.query.findNino
+  const search = nino ? nino.toUpperCase() : ''
+  res.render(`${req.feature}/${req.sprint}/find-a-claim/find-a-claim`, {search, d})
+})
+
+// -----------------------------------------------------------------------------
+// Start a new claim -----------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/start-a-new-claim/', (req, res) => {
+  res.render(`${req.feature}/${req.sprint}/capture/claim-details`)
+})
+
+// -----------------------------------------------------------------------------
+// Schedule --------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/schedule/:scenario', (req, res) => {
+  const scenario = req.params.scenario
+  const d = require(`./_dummy-data/${scenario}.json`)
+  const schedule = require(`./_dummy-data/_schedule.json`)
+  res.render(`${req.feature}/${req.sprint}/schedule/schedule`, {d, schedule})
+})
+
+module.exports = router
