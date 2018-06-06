d24b095640769c7cd68cbb710a3341aba3ec499d
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-06 12:59:40 +0100

    Added version 3 - change payment details

---
+++ app/views/integration/version-3/routes.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const path = require('path')
+const router = new express.Router()
+const {logOnPost} = require('../../../../lib/utils')
+const {addToLog, getTestDate} = require('./functions')
+
+// Log session to console on POST requests
+router.use(logOnPost)
+
+// Set path for PDF's
+router.use('/load-pdf/', express.static(path.join(__dirname, './_pdf')))
+
+router.use((req, res, next) => {
+  res.locals.todayDate = getTestDate()
+  next()
+})
+
+router.get('/', (req, res) => {
+  res.redirect(`/${req.feature}/${req.sprint}/start-a-new-claim/1`)
+})
+
+// -----------------------------------------------------------------------------
+// Find a claim ----------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/find-a-claim/:scenario', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  const d = require(`./_dummy-data/${scenario}.json`)
+  const nino = req.query.findNino
+  const search = nino ? nino.toUpperCase() : ''
+  res.render(`${req.feature}/${req.sprint}/find-a-claim/find-a-claim`, {scenario, search, d})
+})
+
+// -----------------------------------------------------------------------------
+// Start a new claim -----------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/start-a-new-claim/:scenario', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  res.render(`${req.feature}/${req.sprint}/capture/claim-details`, {scenario, start: true})
+})
+router.post('/start-a-new-claim/:scenario', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  if (scenario === '1' || scenario === '2') {
+    return res.redirect(`/${req.feature}/${req.sprint}/task-list/${scenario}`)
+  }
+  res.redirect(`/${req.feature}/${req.sprint}/cis/cis-warning/${scenario}`)    
+})
+
+// -----------------------------------------------------------------------------
+// Task list -------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/task-list/:scenario', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  const d = require(`./_dummy-data/${scenario}.json`)
+  res.render(`${req.feature}/${req.sprint}/task-list/task-list`, {scenario, d})
+})
+
+// -----------------------------------------------------------------------------
+// Confirm details -------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/confirm-details/:scenario', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  const d = require(`./_dummy-data/${scenario}.json`)
+  res.render(`${req.feature}/${req.sprint}/confirm-details/confirm-details`, {scenario, d})
+})
+router.post('/confirm-details/:scenario', (req, res) => {
+  const scenario = req.params.scenario || '1'
+  res.redirect(`/${req.feature}/${req.sprint}/decisions/${scenario}/allowed`)
+})
+
+// -----------------------------------------------------------------------------
+// Completed claims ------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/claim/:scenario/:decision', (req, res) => {
+  const decision = req.params.decision
+  const scenario = req.session.data.scenario || '1'
+  const d = require(`./_dummy-data/${scenario}.json`)
+  res.render(`${req.feature}/${req.sprint}/completed-claim/${decision}`, {scenario, decision, d})
+})
+
+// -----------------------------------------------------------------------------
+// Schedule --------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+router.get('/schedule/:scenario', (req, res) => {
+  const scenario = req.params.scenario
+  const d = require(`./_dummy-data/${scenario}.json`)
+  const schedule = require(`./_dummy-data/_schedule.json`)
+  res.render(`${req.feature}/${req.sprint}/schedule/schedule`, {scenario, d, schedule})
+})
+
+module.exports = router
