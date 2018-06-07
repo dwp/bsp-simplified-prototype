fefdfde74ebefc3cf8edfe7964ad638269a58657
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-06-07 13:02:00 +0100

    Fixed bug

--- app/views/claim-capture-list/version-17/routes.js
+++ app/views/claim-capture-list/version-17/routes.js
@@ -35,8 +35,7 @@ router.get('/start-a-new-claim/', (req, res) => {
 router.get('/schedule/:scenario', (req, res) => {
   const scenario = req.params.scenario
   const d = require(`./_dummy-data/${scenario}.json`)
-  const schedule = require(`./_dummy-data/_schedule.json`)
-  res.render(`${req.feature}/${req.sprint}/schedule/schedule`, {d, schedule})
+  res.render(`${req.feature}/${req.sprint}/schedule/schedule`, {d})
 })
 
 module.exports = router
