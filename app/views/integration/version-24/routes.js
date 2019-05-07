e27ff7f2adb05faf535963e26d77295b4e2fde46
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2019-05-07 16:15:33 +0100

    Updated scenarios for testing

--- app/views/integration/version-24/routes.js
+++ app/views/integration/version-24/routes.js
@@ -50,6 +50,9 @@ router.get('/start-a-new-claim/:scenario', (req, res) => {
 })
 router.post('/start-a-new-claim/:scenario', (req, res) => {
   const scenario = req.params.scenario || '1'
+  if (scenario === '4') {
+    return res.redirect(`/${req.feature}/${req.sprint}/cis/cis-warning/${scenario}`)
+  }
   res.redirect(`/${req.feature}/${req.sprint}/task-list/${scenario}`)
 })
 
